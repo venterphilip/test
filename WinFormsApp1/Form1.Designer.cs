56924ccbc7b7d7b3bae1a27eb5f7d6025cf9585e
philip <philipventer1996@gmail.com>
2022-08-22 18:19:11 +0200

    background color

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -44,6 +44,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.ClientSize = new System.Drawing.Size(958, 528);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
