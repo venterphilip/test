229ac2576613b1e4a4bf4bfb3422015944bab055
philip <philipventer1996@gmail.com>
2022-08-22 18:17:02 +0200

    Button naam updated

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -37,7 +37,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "click me";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
